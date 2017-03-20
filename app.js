c6db13bcc75c7fbd3c10982a72520d1d7beea264
padma <padmab@k3informatics.com>
2017-03-20 08:13:29 +0530

    17-03-2017

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "kiran";
+var lastname='kiran';
+var count=100000000000000000000000000000;
+var price =1001.15;
+var iLikeThisSession= false;//boolean
+var users =[];//Array.
+var person ={};
+
+users=["kiran",1,false];
+person.firstname ="Ravi";
+person.age=30;
