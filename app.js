2cc6d447880562ee8094d2781c0ea9cb79c8761f
padma <padmab@k3informatics.com>
2017-03-22 09:06:17 +0530

    add events

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
-//Task
-//Write data to text box.
-//1.get the reference of the textbox.
 (function(){
- var firstNumber,secondNumber,password;
-    firstNumber = document.getElementById("txtFirstNumber");
-    secondNumber= document.getElementById("txtSecondNumber");
-    password = document.getElementById("txtPassword");
+    var fName,lName,age,btnRegister;
+    fName= document.getElementById("txtFirstName");
+    lName= document.getElementById("txtLastName");
+    age= document.getElementById("txtAge");
+    btnRegister = document.getElementById("btnRegister");
     
-    console.log(firstNumber);
-    firstNumber.value=10;
-    secondNumber.value=30;
+   /* fName.value="kiran";
+    lName.value="PVS";
+    age.value="30";*/
     
-    var typeOfPassword = password.type;
     
-    password.type="date";
-    typeOfPassword = password.type;
-    password.className="trynew";
-    password.id="newPassword";
+    
+    function registerUser(evt){
+        var user ={};//object
+        user.firstName = fName.value;
+        user.lastName =lName.value;
+        user.age =age.value;
+        
+        console.log(user);
+        console.log(evt);
+    }
+    
+    function numbersOnly(evt){
+        console.log("age entered");
+        console.log(evt);
+        var enteredKey = evt.keyCode;
+        if(enteredKey>=48 && enteredKey<=57){
+            console.log("valid");
+        }
+        else{
+            evt.preventDefault();
+        }
+    }
+    function alphabetsOnly(evt){
+        var enteredKey = evt.keyCode;
+        console.log(enteredKey);
+        if( 
+            (enteredKey>=97 && enteredKey<=122) ||
+            (enteredKey>=65 && enteredKey<=90)
+            ){
+            console.log("valid character");
+        }
+        else{
+            evt.preventDefault();
+        }
+    }
+    btnRegister.addEventListener("click",registerUser);
+    age.addEventListener("keypress",numbersOnly);
+    fName.addEventListener("keypress",alphabetsOnly);
+    
 })();
 
 
-(function(){
-    var elements;
-    elements = document.getElementsByTagName("input");
-    console.log(elements);
-})();
 
 
 
 
-(function(){
-    var elements;
-    elements = document.getElementsByClassName("abcd");
-    console.log(elements);
-})();
+
+
+
